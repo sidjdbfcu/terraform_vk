2e8d2689d056cf6c5288e75a77cc98878b5f2f37
sidjdbfcu <164883478+sidjdbfcu@users.noreply.github.com>
2025-06-03 22:50:29 +0300

    Update vkcs_provider.tf

--- vkcs_provider.tf
+++ vkcs_provider.tf
@@ -8,16 +8,9 @@ terraform {
 }
 
 provider "vkcs" {
-    # Your user account.
     username = "-"
-
-    # The password of the account
     password = "-"
-
     project_id = "-"
-
-    # Region name
     region = "RegionOne"
-
     auth_url = "https://infra.mail.ru:35357/v3/"
 }
